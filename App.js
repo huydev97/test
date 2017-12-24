a5ed5682d196cf271c5b21340d655b52d3023b05
Nguyen Van Nhat Huy <huydev97@gmail.com>
2017-12-24 13:56:28 +0700

    asd

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  PermissionsAndroid
+} from 'react-native';
+const bac = Platform.select({
+  ios: 'Đây là ios',
+  android: 'Đây là điện thoại android'
+})
+export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{bac}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
